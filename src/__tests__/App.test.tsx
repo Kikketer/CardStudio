2bb2c4befa0d68859c6ce4a49a0b4caf7bceb13a
Chris Weed <chris@cjweed.com>
2021-03-26 17:10:22 -0500

    Fixed a test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import App from '../App'
+
+jest.mock('fabric-browseronly')
 
 describe('App', () => {
   it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
-});
+    expect(render(<App />)).toBeTruthy()
+  })
+})
