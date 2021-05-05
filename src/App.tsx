d86c8b713287cc8721a3435ef62890fa9f686e75
Chris Weed <chris@cjweed.com>
2021-05-04 23:14:34 -0500

    Simple update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
-import Editor from './components/Editor'
-import Toolbar from './components/Toolbar'
-import Detailbar from './components/Detailbar'
+// import Editor from './components/Editor'
+// import Toolbar from './components/Toolbar'
+// import Detailbar from './components/Detailbar'
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  HeaderContainer,
+  SideNav,
+  SideNavItems,
+  SideNavMenu,
+  SideNavMenuItem,
+  SideNavLink,
+} from 'carbon-components-react'
+import { Cursor_132, Search20, Fade16, Notification20, AppSwitcher20 } from '@carbon/icons-react'
 import { DeckProvider } from './components/Deck.context'
 import './app.scss'
 
@@ -16,11 +29,30 @@ const Grid = styled.div`
 const Frame = () => {
   return (
     <DeckProvider>
-      <Grid>
-        <Toolbar />
-        <Editor />
-        <Detailbar />
-      </Grid>
+      <HeaderContainer
+        render={() => (
+          <Header aria-label="Card Studio">
+            <HeaderName href="#" prefix="Card">
+              [Studio]
+            </HeaderName>
+            <HeaderGlobalBar>
+              <HeaderGlobalAction aria-label="Search" onClick={() => console.log('search click')}>
+                <Search20 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction aria-label="Notifications" onClick={() => console.log('notification click')}>
+                <Notification20 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="App Switcher"
+                onClick={() => console.log('app-switcher click')}
+                tooltipAlignment="end"
+              >
+                <AppSwitcher20 />
+              </HeaderGlobalAction>
+            </HeaderGlobalBar>
+          </Header>
+        )}
+      />
     </DeckProvider>
   )
 }
